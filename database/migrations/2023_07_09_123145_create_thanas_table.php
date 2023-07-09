f0f63061a0ccc7a076f5b123003053ed694ab7e8
tarikulIslamNahid <jttarekulnahid@gmail.com>
2023-07-09 12:32:51 +0600

    thana table add

---
+++ database/migrations/2023_07_09_123145_create_thanas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('thanas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('division_id')->unsigned()->index();
+            $table->bigInteger('district_id')->unsigned()->index();
+            $table->foreign('division_id')->references('id')->on('divisions')->onDelete('cascade');
+            $table->foreign('district_id')->references('id')->on('districts')->onDelete('cascade');
+            $table->string('code',6);
+            $table->string('name_en',50);
+            $table->string('name_bn',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('thanas');
+    }
+};
