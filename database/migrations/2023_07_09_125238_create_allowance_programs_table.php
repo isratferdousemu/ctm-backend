59a7a43cf6c79fa4006649cf494657b7a285c9be
tarikulIslamNahid <jttarekulnahid@gmail.com>
2023-07-09 12:55:53 +0600

    allowance table add

---
+++ database/migrations/2023_07_09_125238_create_allowance_programs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('allowance_programs', function (Blueprint $table) {
+            $table->id();
+            $table->string('code',6);
+            $table->string('name_en',50);
+            $table->string('name_bn',50);
+            $table->string('guideline',120)->nullable();
+            $table->integer('service_type',10);
+            $table->string('description',120)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('allowance_programs');
+    }
+};
