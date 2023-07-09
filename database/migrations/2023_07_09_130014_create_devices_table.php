c11ac077997407a0b192fff75eee41e2e3331e15
tarikulIslamNahid <jttarekulnahid@gmail.com>
2023-07-09 13:04:33 +0600

    tables add

---
+++ database/migrations/2023_07_09_130014_create_devices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name',50);
+            $table->integer('device_type',10);
+            $table->string('device_name',30);
+            $table->macAddress();
+            $table->string('username_mapping',50);
+            $table->string('purpose_use',120)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
