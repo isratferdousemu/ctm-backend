e77e980466cea166e280ea4509e7f9966ed9f36d
aminuribcs <aminur.rashid@ibcs-primax.com>
2023-09-21 14:18:22 +0600

    backend: permission seeder page url and page parent setup

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -31,104 +31,131 @@ class PermissionSeeder extends Seeder
             'sub_module_name' => $this->subDemographicInformationManagement,
             'guard_name' => $this->guard,
             'permissions'=>[
-                'division-create',
-                'division-list',
-                'division-edit',
-                'division-view',
-                'division-delete',
-                'district-create',
-                'district-list',
-                'district-edit',
-                'district-view',
-                'district-delete',
-                'city-create',
-                'city-list',
-                'city-edit',
-                'city-view',
-                'city-delete',
-                'thana-create',
-                'thana-list',
-                'thana-edit',
-                'thana-view',
-                'thana-delete',
-                'union-create',
-                'union-list',
-                'union-edit',
-                'union-view',
-                'union-delete',
-                'ward-create',
-                'ward-list',
-                'ward-edit',
-                'ward-view',
-                'ward-delete',
-                        ]
+                ["name"=>"division-create", "page_url"=>"/system-configuration/division/create", "parent_page"=>1],
+                ["name"=>"division-list", "page_url"=>"/system-configuration/division", "parent_page"=>1],
+                ["name"=>"division-edit", "page_url"=>"/system-configuration/division/edit/:id", "parent_page"=>1],
+                ["name"=>"division-view", "page_url"=>"/system-configuration/division/details/:id", "parent_page"=>1],
+                ["name"=>"division-delete", "page_url"=>"/system-configuration/division", "parent_page"=>1],
+
+                ["name"=>"district-create", "page_url"=>"/system-configuration/district/create", "parent_page"=>1],
+                ["name"=>"district-list", "page_url"=>"/system-configuration/district", "parent_page"=>1],
+                ["name"=>"district-edit", "page_url"=>"/system-configuration/district/edit/:id", "parent_page"=>1],
+                ["name"=>"district-view", "page_url"=>"/system-configuration/district/details/:id", "parent_page"=>1],
+                ["name"=>"district-delete", "page_url"=>"/system-configuration/district", "parent_page"=>1],
+
+                ["name"=>"city-create", "page_url"=>"/system-configuration/city/create", "parent_page"=>1],
+                ["name"=>"city-list", "page_url"=>"/system-configuration/city", "parent_page"=>1],
+                ["name"=>"city-edit", "page_url"=>"/system-configuration/city/edit/:id", "parent_page"=>1],
+                ["name"=>"city-view", "page_url"=>"/system-configuration/city/details/:id", "parent_page"=>1],
+                ["name"=>"city-delete", "page_url"=>"/system-configuration/city", "parent_page"=>1],
+
+                ["name"=>"thana-create", "page_url"=>"/system-configuration/thana/create", "parent_page"=>1],
+                ["name"=>"thana-list", "page_url"=>"/system-configuration/thana", "parent_page"=>1],
+                ["name"=>"thana-edit", "page_url"=>"/system-configuration/thana/edit/:id", "parent_page"=>1],
+                ["name"=>"thana-view", "page_url"=>"/system-configuration/thana/details/:id", "parent_page"=>1],
+                ["name"=>"thana-delete", "page_url"=>"/system-configuration/thana", "parent_page"=>1],
+
+                ["name"=>"union-create", "page_url"=>"/system-configuration/union/create", "parent_page"=>1],
+                ["name"=>"union-list", "page_url"=>"/system-configuration/union", "parent_page"=>1],
+                ["name"=>"union-edit", "page_url"=>"/system-configuration/union/edit/:id", "parent_page"=>1],
+                ["name"=>"union-view", "page_url"=>"/system-configuration/union/details/:id", "parent_page"=>1],
+                ["name"=>"union-delete", "page_url"=>"/system-configuration/union", "parent_page"=>1],
+
+                ["name"=>"ward-create", "page_url"=>"/system-configuration/ward/create", "parent_page"=>1],
+                ["name"=>"ward-list", "page_url"=>"/system-configuration/ward", "parent_page"=>1],
+                ["name"=>"ward-edit", "page_url"=>"/system-configuration/ward/edit/:id", "parent_page"=>1],
+                ["name"=>"ward-view", "page_url"=>"/system-configuration/ward/details/:id", "parent_page"=>1],
+                ["name"=>"ward-delete", "page_url"=>"/system-configuration/ward", "parent_page"=>1]
+            ]
         ],
         [
             'module_name' => $this->modulePermissionSystemConfiguration,
             'sub_module_name' => $this->subAllowanceProgramManagement,
             'guard_name' => $this->guard,
             'permissions'=>[
+                ["name"=>"allowance-create", "page_url"=>"/system-configuration/allowance/create", "parent_page"=>0],
+                ["name"=>"allowance-list", "page_url"=>"/system-configuration/allowance", "parent_page"=>0],
+                ["name"=>"allowance-edit", "page_url"=>"/system-configuration/allowance/edit/:id", "parent_page"=>0],
+                ["name"=>"allowance-view", "page_url"=>"/system-configuration/allowance/details/:id", "parent_page"=>0],
+                ["name"=>"allowance-delete", "page_url"=>"/system-configuration/allowance", "parent_page"=>0],
+                ["name"=>"allowance-field-create", "page_url"=>"/system-configuration/allowance/create/field", "parent_page"=>0],
                 'allowance-create',
                 'allowance-list',
                 'allowance-edit',
                 'allowance-view',
-                'allowance-delete'
-                        ]
+                'allowance-delete',
+                'allowance-field-create'
+            ]
         ],
         [
             'module_name' => $this->modulePermissionSystemConfiguration,
             'sub_module_name' => $this->subOfficeInformationManagement,
             'guard_name' => $this->guard,
             'permissions'=>[
-                'office-create',
-                'office-list',
-                'office-edit',
-                'office-view',
-                'office-delete'
-                        ]
+                ["name"=>"office-create", "page_url"=>"/system-configuration/office/create", "parent_page"=>0],
+                ["name"=>"office-list", "page_url"=>"/system-configuration/office", "parent_page"=>0],
+                ["name"=>"office-edit", "page_url"=>"/system-configuration/office/edit/:id", "parent_page"=>0],
+                ["name"=>"office-view", "page_url"=>"/system-configuration/office/details/:id", "parent_page"=>0],
+                ["name"=>"office-delete", "page_url"=>"/system-configuration/office", "parent_page"=>0]
+            ]
         ],
         [
             'module_name' => $this->modulePermissionSystemConfiguration,
             'sub_module_name' => $this->subFinancialInformationManagement,
             'guard_name' => $this->guard,
             'permissions'=>[
-                'financial-create',
-                'financial-list',
-                'financial-edit',
-                'financial-view',
-                'financial-delete'
-                        ]
+                ["name"=>"financial-create", "page_url"=>"/system-configuration/financial/create", "parent_page"=>0],
+                ["name"=>"financial-list", "page_url"=>"/system-configuration/financial", "parent_page"=>0],
+                ["name"=>"financial-edit", "page_url"=>"/system-configuration/financial/edit/:id", "parent_page"=>0],
+                ["name"=>"financial-view", "page_url"=>"/system-configuration/financial/details/:id", "parent_page"=>0],
+                ["name"=>"financial-delete", "page_url"=>"/system-configuration/financial", "parent_page"=>0]
+            ]
         ],
         [
             'module_name' => $this->modulePermissionSystemConfiguration,
             'sub_module_name' => $this->subUserManagement,
             'guard_name' => $this->guard,
             'permissions'=>[
-                'role-create',
-                'role-list',
-                'role-edit',
-                'role-view',
-                'role-delete',
-                'role-permission-create',
-                'user-create',
-                'user-list',
-                'user-edit',
-                'user-view',
-                'user-delete',
-                        ]
+                ["name"=>"role-create", "page_url"=>"/system-configuration/role/create", "parent_page"=>1],
+                ["name"=>"role-list", "page_url"=>"/system-configuration/role", "parent_page"=>1],
+                ["name"=>"role-edit", "page_url"=>"/system-configuration/role/edit/:id", "parent_page"=>1],
+                ["name"=>"role-view", "page_url"=>"/system-configuration/role/details/:id", "parent_page"=>1],
+                ["name"=>"role-delete", "page_url"=>"/system-configuration/role", "parent_page"=>1],
+
+                ["name"=>"role-permission-create", "page_url"=>"/system-configuration/role-permission/create", "parent_page"=>1],
+
+                ["name"=>"user-create", "page_url"=>"/system-configuration/user/create", "parent_page"=>1],
+                ["name"=>"user-list", "page_url"=>"/system-configuration/user", "parent_page"=>1],
+                ["name"=>"user-edit", "page_url"=>"/system-configuration/user/edit/:id", "parent_page"=>1],
+                ["name"=>"user-view", "page_url"=>"/system-configuration/user/details/:id", "parent_page"=>1],
+                ["name"=>"user-delete", "page_url"=>"/system-configuration/user", "parent_page"=>1]
+            ]
         ],
         [
             'module_name' => $this->modulePermissionSystemConfiguration,
             'sub_module_name' => $this->subDeviceRegistrationManagement,
             'guard_name' => $this->guard,
             'permissions'=>[
-                'device-create',
-                'device-list',
-                'device-edit',
-                'device-view',
-                'device-delete'
-                        ]
+                ["name"=>"device-create", "page_url"=>"/system-configuration/device/create", "parent_page"=>0],
+                ["name"=>"device-list", "page_url"=>"/system-configuration/device", "parent_page"=>0],
+                ["name"=>"device-edit", "page_url"=>"/system-configuration/device/edit/:id", "parent_page"=>0],
+                ["name"=>"device-view", "page_url"=>"/system-configuration/device/details/:id", "parent_page"=>0],
+                ["name"=>"device-delete", "page_url"=>"/system-configuration/device", "parent_page"=>0]
+            ]
         ],
+        [
+            'module_name' => $this->modulePermissionSystemConfiguration,
+            'sub_module_name' => $this->menuManagement,
+            'guard_name' => $this->guard,
+            'permissions' => [
+                ["name"=> "menu-create", "page_url"=>"/system-configuration/menu/create", "parent_page"=>0],
+                ["name"=> "menu-list", "page_url"=>"/system-configuration/menu", "parent_page"=>0],
+                ["name"=> "menu-edit", "page_url"=>"/system-configuration/menu/edit/:id", "parent_page"=>0],
+                ["name"=> "menu-view", "page_url"=>"/system-configuration/menu/details/:id", "parent_page"=>0],
+                ["name"=> "menu-delete", "page_url"=>"/system-configuration/menu", "parent_page"=>0]
+            ]
+        ],
+
         /* -------------------------------------------------------------------------- */
         /*                            Application Selection                           */
         /* -------------------------------------------------------------------------- */
@@ -346,21 +373,21 @@ class PermissionSeeder extends Seeder
 
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-                    for ($i=0; $i < count($permissions) ; $i++) {
-                        $groupPermissions=$permissions[$i]['module_name'];
-                        $subModulePermissions=$permissions[$i]['sub_module_name'];
-                        $guardPermissions=$permissions[$i]['guard_name'];
-                        for ($j=0; $j < count($permissions[$i]['permissions']); $j++) {
-                        //create permissions
-                        $permission = Permission::create([
-                            'name' => $permissions[$i]['permissions'][$j],
-                            'module_name' => $groupPermissions,
-                            'sub_module_name' => $subModulePermissions,
-                            'guard_name' => $guardPermissions,
-                            ]);
+            for ($i=0; $i < count($permissions) ; $i++) {
+                $groupPermissions=$permissions[$i]['module_name'];
+                $subModulePermissions=$permissions[$i]['sub_module_name'];
+                $guardPermissions=$permissions[$i]['guard_name'];
+                for ($j=0; $j < count($permissions[$i]['permissions']); $j++) {
+                //create permissions
+                $permission = Permission::create([
+                    'name' => $permissions[$i]['permissions'][$j],
+                    'module_name' => $groupPermissions,
+                    'sub_module_name' => $subModulePermissions,
+                    'guard_name' => $guardPermissions,
+                    ]);
 
-                        }
+                }
 
-                        }
+            }
     }
 }
