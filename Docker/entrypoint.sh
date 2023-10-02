b8488c4abaa05ad27ce1f165feeacbefca43338a
mostofa <mostofa.kamal@ibcs-primax.com>
2023-10-02 16:59:53 +0600

    deployment process

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-progress --no-interaction --ignore-platform-reqs
-fi
+composer install --no-progress --no-interaction --ignore-platform-reqs
 
-#composer install --no-progress --no-interaction --ignore-platform-reqs
-
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
-    cp .env.example .env
-else
-    echo "env file exists."
-fi
-
-#echo "Creating env file for env $APP_ENV"
-#cp .env.example .env
+echo "Creating env file for env $APP_ENV"
+cp .env.example .env
 
 php artisan migrate:fresh --seed
-#php artisan db:seed --class=PermissionTableSeeder
-#php artisan db:seed --class=UserSeeder
 php artisan key:generate
 php artisan route:clear
 php artisan config:clear
