78dc758eef70f648ed881e84ae7bf7fde212a87e
aminuribcs <aminur.rashid@ibcs-primax.com>
2023-10-05 11:16:00 +0600

    backend: permission and route configuration

--- routes/Admin/SystemConfig/UserRoutes.php
+++ routes/Admin/SystemConfig/UserRoutes.php
@@ -6,10 +6,10 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::prefix('admin/user')->group(function () {
 
-        Route::post('/insert', [UserController::class, 'insertUser'])->middleware(['role_or_permission:super-admin|demo-graphic-create']);
-        // Route::get('/get',[LocationController::class, 'getAllDivisionPaginated'])->middleware(['role_or_permission:super-admin|demo-graphic-view']);
-        // Route::post('/update', [LocationController::class, 'divisionUpdate'])->middleware(['role_or_permission:super-admin|demo-graphic-update']);
-        // Route::get('/destroy/{id}', [LocationController::class, 'destroyDivision'])->middleware(['role_or_permission:super-admin|demo-graphic-destroy']);
+        Route::post('/insert', [UserController::class, 'insertUser'])->middleware(['role_or_permission:super-admin|user-create']);
+         Route::get('/get',[UserController::class, 'getAllDivisionPaginated'])->middleware(['role_or_permission:super-admin|user-view']);
+         Route::post('/update', [UserController::class, 'divisionUpdate'])->middleware(['role_or_permission:super-admin|user-update']);
+         Route::get('/destroy/{id}', [UserController::class, 'destroyDivision'])->middleware(['role_or_permission:super-admin|user-delete']);
     });
 
 
