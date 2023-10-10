aea06b7f8483ee33cec9543fe562f72740a8d109
tarikulIslamNahid <jttarekulnahid@gmail.com>
2023-10-10 15:23:23 +0600

    committee update api done

--- app/Http/Services/Admin/Beneficiary/BeneficiaryService.php
+++ app/Http/Services/Admin/Beneficiary/BeneficiaryService.php
@@ -103,13 +103,33 @@ class BeneficiaryService
         try {
             $committee                         = Committee::find($request->id);
             $committee->code                   = $request->code;
-            $committee->name                   = $request->name;
             $committee->details                = $request->details;
-            $committee->program_id             = $request->program_id;
-            $committee->division_id            = $request->division_id;
-            $committee->district_id            = $request->district_id;
-            $committee->office_id              = $request->office_id;
-            $committee->location_id            = $request->location_id;
+            $committee->committee_type             = $request->committee_type;
+            $committee->program_id              = $request->program_id;
+            if ($request->has('committee_type')) {
+                if ($request->committee_type != 18 || $request->committee_type != 19) {
+                    if ($request->committee_type == 12 && $request->has('union_id')) {
+                        $committee->location_id = $request->union_id;
+                    }
+                    else if ($request->committee_type == 13 && $request->has('ward_id')) {
+                        $committee->location_id = $request->ward_id;
+                    }
+                    else if ($request->committee_type == 14 && $request->has('upazila_id')) {
+                        $committee->location_id = $request->upazila_id;
+                    }
+                    else if ($request->committee_type == 15 && $request->has('city_corpo_id')) {
+                        $committee->location_id = $request->city_corpo_id;
+                    }
+                    else if ($request->committee_type == 16 && $request->has('paurashava_id')) {
+                        $committee->location_id = $request->paurashava_id;
+                    }
+                    else if ($request->committee_type == 17 && $request->has('district_id')) {
+                        $committee->location_id = $request->district_id;
+                    }
+                }
+            }
+            $committee->name = $this->committeeName($request->committee_type, $request->program_id, $committee->location_id);
+
             $committee ->save();
 
             CommitteeMember::whereCommittee_id($request->id)->delete();
@@ -122,7 +142,7 @@ class BeneficiaryService
                  $member                     =  new CommitteeMember;
                  $member->committee_id        = $committee->id;
                  $member->member_name    	 = $item['member_name'];
-                 $member->designation    	 = $item['designation'];
+                 $member->designation    	 = $item['designation_id'];
                  $member->email    	         = $item['email'];
                  $member->address    	     = $item['address'];
                  $member->phone              = $item['phone'];
