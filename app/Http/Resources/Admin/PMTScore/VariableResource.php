ddcc3cbd036083b67bba6a4040eed947c9452755
tarikulIslamNahid <jttarekulnahid@gmail.com>
2023-10-26 11:50:16 +0600

    online application frontend design done

--- app/Http/Resources/Admin/PMTScore/VariableResource.php
+++ app/Http/Resources/Admin/PMTScore/VariableResource.php
@@ -14,6 +14,12 @@ class VariableResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'               =>      $this->id,
+            'name_en'             =>      $this->name_en,
+            'field_type'        =>      $this->field_type,
+            'parent_id'         =>      $this->parent_id,
+            'children'          =>  VariableResource::collection($this->whenLoaded('children'))
+        ];
     }
 }
