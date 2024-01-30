4297f80b7a8883f47d831194ce5d659b792af11e
tarikool <ijad.tarikool@gmail.com>
2024-01-30 14:32:12 +0600

    working on chunk pdf

--- resources/views/reports/city.blade.php
+++ resources/views/reports/city.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>উপজেলা/সিটি কর্পোরেশন/জেলা পৌরসভা তালিকা</title>
 
     <style>
         body {
@@ -100,21 +100,21 @@
     <thead>
     <tr>
         <th style="width: 10%;">ক্রমিক নং </th>
-     
+
             <th>বিভাগ</th>
             <th>জেলা</th>
             <th>উপজেলা/সিটি কর্পোরেশন/জেলা পৌরসভা</th>
-   
+
     </tr>
     </thead>
     <tbody>
     @php($count = 0);
-    @foreach($applications as $item)
+    @foreach($items as $row)
         <tr>
             <td>{{++$count}}</td>
-            @foreach($columns as $col)
-                <td>{{$item[$col]}}</td>
-            @endforeach
+            <td>{{$row->parent?->parent?->name_bn}}</td>
+            <td>{{$row->parent?->name_bn}}</td>
+            <td>{{$row->name_bn}}</td>
         </tr>
     @endforeach
     </tbody>
--- resources/views/reports/union.blade.php
+++ resources/views/reports/union.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>থানা/ইউনিয়ন/পৌরসভা তালিকা</title>
 
     <style>
         body {
@@ -100,22 +100,23 @@
     <thead>
     <tr>
         <th style="width: 10%;">ক্রমিক নং </th>
-     
+
             <th>বিভাগ</th>
             <th>জেলা</th>
             <th>উপজেলা/সিটি কর্পোরেশন/জেলা পৌরসভা</th>
             <th>থানা/ইউনিয়ন/পৌরসভা</th>
-   
+
     </tr>
     </thead>
     <tbody>
     @php($count = 0);
-    @foreach($applications as $item)
+    @foreach($items as $row)
         <tr>
             <td>{{++$count}}</td>
-            @foreach($columns as $col)
-                <td>{{$item[$col]}}</td>
-            @endforeach
+            <td>{{$row->parent?->parent?->parent?->name_bn}}</td>
+            <td>{{$row->parent?->parent?->name_bn}}</td>
+            <td>{{$row->parent?->name_bn}}</td>
+            <td>{{$row->name_bn}}</td>
         </tr>
     @endforeach
     </tbody>
