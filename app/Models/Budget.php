c8466cf94a95474d994f0db4adf941536cbef8bc
Sirajul Islam <hasannbd@gmail.com>
2024-03-24 21:40:26 +0600

    budget added

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -4,21 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Budget extends Model
 {
-    use HasFactory, SoftDeletes;
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [];
-    /**
-     * The attributes that aren't mass assignable.
-     *
-     * @var array
-     */
-    protected $guarded = ['id'];
+    use HasFactory;
 }
