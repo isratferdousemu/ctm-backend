bbaf0e9ced06e8a26bcec49c9e943fa9a5eddbb5
tarikool <ijad.tarikool@gmail.com>
2024-04-02 11:12:07 +0600

    Route added

--- routes/Admin/APIManager/APIManagerRoute.php
+++ routes/Admin/APIManager/APIManagerRoute.php
@@ -2,12 +2,13 @@
 
 
 use App\Http\Controllers\Api\V1\Admin\APIURLController;
+use App\Http\Controllers\APIController;
 
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::prefix('admin')->group(function () {
-        Route::apiResource('api-url', APIURLController::class)
-            /*->parameter('api-url', 'apiUrl')*/;
+        Route::apiResource('api-url', APIURLController::class);
+        Route::apiResource('api', APIController::class);
     });
 
 
