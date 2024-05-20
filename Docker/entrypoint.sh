0a3ebe7109972afdddf844afb555178b03d7f5de
Md Atikur Rahman <atikur.rahman@ibcs-primax.com>
2024-05-20 10:55:56 +0000

    Update entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -23,6 +23,7 @@ if [ "$role" = "app" ]; then
     php artisan db:seed --class=InstallmentSeeder
     php artisan storage:link
     php artisan schedule:run
+    php artisan queue:work --queue=high,default
 
 
     php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
