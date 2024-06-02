839f8624ab766afd421723119a40edf620b448a3
unknown <shohorab.ahmed@ibcs-primax.com>
2024-06-02 18:11:00 +0600

    changes in api for mobile

--- routes/Mobile/PayrollManagement/PayrollRoute.php
+++ routes/Mobile/PayrollManagement/PayrollRoute.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin\PaymentProcessorController;
 use App\Http\Controllers\Api\V1\Admin\PayrollSettingController;
 
 Route::middleware('auth:sanctum')->group(function () {
     /* -------------------------------------------------------------------------- */
     /*                       Payroll setting Routes                         */
     /* -------------------------------------------------------------------------- */
-    Route::prefix('admin/payroll')->group(function () {
+    Route::prefix('mobile/payroll')->group(function () {
         //for payroll setting
         Route::get('/get-all-allowance', [PayrollSettingController::class, 'getAllAllowance'])->middleware(['role_or_permission:super-admin|payroll-setting-view']);
         Route::get('/get-financial-year', [PayrollSettingController::class, 'getFinancialYear'])->middleware(['role_or_permission:super-admin|payroll-setting-view']);
@@ -16,6 +17,8 @@ Route::middleware('auth:sanctum')->group(function () {
         //for payroll verification
         Route::post('/verification-setting-submit', [PayrollSettingController::class, 'payrollVerification']);
         Route::get('/get-verification-setting', [PayrollSettingController::class, 'getVerificationSetting']);
+        // beneficiary tracking information
+        Route::post('/payment-tracking-info', [PaymentProcessorController::class, 'getPaymentTrackingInfo']);
 
 
 
