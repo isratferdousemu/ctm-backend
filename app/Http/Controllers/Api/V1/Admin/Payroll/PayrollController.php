07fdc5ddb5a7df55ae9d10bc80c77059f2bb0f2e
Sirajul Islam <hasannbd@gmail.com>
2024-06-05 06:20:52 +0600

    payroll create

---
+++ app/Http/Controllers/Api/V1/Admin/Payroll/PayrollController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin\Payroll;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\Payroll\AllotmentResource;
+use App\Http\Resources\Admin\Payroll\PayrollInstallmentScheduleResource;
+use App\Http\Services\Admin\Beneficiary\BeneficiaryService;
+use App\Http\Services\Admin\Payroll\PayrollService;
+use App\Http\Traits\MessageTrait;
+use Illuminate\Http\Request;
+
+class PayrollController extends Controller
+{
+    use MessageTrait;
+
+    /**
+     * @var PayrollService
+     */
+    private PayrollService $payrollService;
+
+    /**
+     * @param PayrollService $payrollService
+     */
+    public function __construct(PayrollService $payrollService)
+    {
+        $this->payrollService = $payrollService;
+    }
+
+    /**
+     * @param int $program_id
+     * @param int $financial_year_id
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function getActiveInstallments(int $program_id, int $financial_year_id): \Illuminate\Http\JsonResponse|\Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    {
+        try {
+            $activeInstallmentList = $this->payrollService->getActiveInstallments($program_id, $financial_year_id);
+//            return response()->json($beneficiaryList);
+            return PayrollInstallmentScheduleResource::collection($activeInstallmentList)->additional([
+                'success' => true,
+                'message' => $this->fetchSuccessMessage,
+            ]);
+        } catch (\Throwable $th) {
+            return $this->sendError($th->getMessage(), [], 500);
+        }
+    }
+
+    public function getAllotmentAreaList(Request $request): \Illuminate\Http\JsonResponse|\Illuminate\Http\Resources\Json\AnonymousResourceCollection
+    {
+        try {
+            $allotmentAreaList = $this->payrollService->getAllotmentAreaList($request);
+//            return response()->json($beneficiaryList);
+            return AllotmentResource::collection($allotmentAreaList)->additional([
+                'success' => true,
+                'message' => $this->fetchSuccessMessage,
+            ]);
+        } catch (\Throwable $th) {
+            return $this->sendError($th->getMessage(), [], 500);
+        }
+    }
+
+}
